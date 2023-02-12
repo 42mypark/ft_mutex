925c696c7be01d4bca2322b68253f8a1fa4f7986
mypark <mypark@myparkui-MacBookPro.local>
2023-02-12 20:10:58 +0900

    refactor: mutex struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,51 +1,63 @@
 use core::arch::asm;
+use core::marker::Sized;
 
-static mut LOCK_ATOMIC: usize = 0;
-static mut sum: usize = 0;
-
-/// cmpxchg [m1], r1, al
-/// if [m1] == al {
-///     [m1] = r1;
-/// } else {
-///     al = [m1];
-/// }
-
-fn lock() {
-	while unsafe { cmpxchg() } {}
+struct SpinMutex<T: ?Sized> {
+	lock_atomic: usize,
+	pub value: T,
 }
 
-fn unlock() {
-	unsafe { LOCK_ATOMIC = 0 }
-}
-
-unsafe fn cmpxchg() -> bool {
-	let mut al: i8 = 0;
-	let cl: i8 = 1;
-	let rdi = &LOCK_ATOMIC as *const usize;
-	asm!(
-		"lock cmpxchg [rdi], cl",
-		in ("rdi") rdi,
-		in ("cl") cl,
-		inout ("al") al,
-	);
-	al == 1
-}
-
-fn func() {
-	for _ in 0..100000 {
-		lock();
-		unsafe {
-			sum += 1;
+impl<T> SpinMutex<T> {
+	const fn new(value: T) -> Self {
+		SpinMutex {
+			lock_atomic: 0,
+			value,
 		}
-		unlock();
 	}
 
-	unsafe { println!("{sum}") };
+	fn lock(&self) {
+		while unsafe { self.cmpxchg() } {}
+	}
+
+	fn unlock(&mut self) {
+		self.lock_atomic = 0;
+	}
+
+	/// cmpxchg [m1], r1, al
+	/// if [m1] == al {
+	///     [m1] = r1;
+	/// } else {
+	///     al = [m1];
+	/// }
+	unsafe fn cmpxchg(&self) -> bool {
+		let mut al: i8 = 0;
+		let cl: i8 = 1;
+		let rdi = &self.lock_atomic as *const usize;
+		asm!(
+			"lock cmpxchg [rdi], cl",
+			in ("rdi") rdi,
+			in ("cl") cl,
+			inout ("al") al,
+		);
+		al == 1
+	}
 }
 
 #[cfg(test)]
 mod tests {
+	static mut sum: SpinMutex<usize> = SpinMutex::new(0);
 	use super::*;
+
+	fn func() {
+		for _ in 0..100000 {
+			unsafe {
+				sum.lock();
+				sum.value += 1;
+				sum.unlock();
+			}
+		}
+
+		unsafe { println!("{}", sum.value) };
+	}
 
 	#[test]
 	fn it_works() {
